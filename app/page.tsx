2434a3f2c4479c4a0880e261716216fc52791118
Ryan <phoen.ryan@gmail.com>
2024-10-25 14:33:14 +0800

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
+import { AuroraBackground } from "@/components/ui/aurora-background";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import * as React from "react";
+import { motion } from "framer-motion";
 
 export default function RootLayout() {
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
-      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-        <div className="flex flex-col space-y-2 text-center">
-          <h1 className="text-2xl font-semibold tracking-tight">
-            Data Request Access Workflow Demo
-          </h1>
-          <div className="py-5 ">
-            <Button asChild>
-              <Link href="/login">Login</Link>
-            </Button>
-          </div>
-        </div>
+      <div className="text-3xl md:text-6xl font-bold dark:text-white text-center">
+      Data Request Access Workflow Demo
       </div>
+      <div className="font-extralight text-base md:text-3xl dark:text-neutral-200 py-4">
+        Use admin:admin to login
+      </div>
+      <button className="bg-black dark:bg-white rounded-full w-fit text-white dark:text-black px-4 py-2">
+        <Link href="/login">Login</Link>
+      </button>
     </div>
   );
 }
